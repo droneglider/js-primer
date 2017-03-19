4b3f09dee1f4c1b85ed897d9eaec8df35e256873
Martin Swiderski <codebloke@gmail.com>
2017-03-19 10:32:07 +0000

    sumer camp

--- test.js
+++ test.js
@@ -1,30 +1,65 @@
 
 
 
-function howOld(born) {
-    return (new Date).getFullYear() - born;
+
+function summerCourseForGirls3to5yo(children) {
+    var filtered = [];
+    for (var no in children) {
+        if (children[no].age > 2 && children[no].age < 6) {
+            filtered.push(children[no]);
+        }
+    }
+    return filtered;
+}
+
+function summerCourseForBoys5to11(children) {
+    var filtered = [];
+    for (var no in children) {
+        if (children[no].gender === 'm' &&
+            (children[no].age > 4 && children[no].age < 12)) {
+            filtered.push(children[no]);
+        }
+    }
+    return filtered;
+}
+
+function howOld(born, month) {
+    var tempAge = (new Date).getFullYear() - born;
+    if (1 * (new Date).getMonth() < month) {
+        tempAge = tempAge - 1;
+    }
+    return tempAge;
 }
 
 var children = [ // array
     {
+        _yob: 2007,
+        _mob: 1,
         name: 'Julia',
-        yob: 2007,
+        gender: 'f',
         age: -1
     },
     {
+        _yob: 2009,
+        _mob: 8,
         name: 'Leon',
-        yob: 2009,
+        gender: 'm',
         age: -1
     },
     {
+        _yob: 2011,
+        _mob: 11,
         name: 'Rita',
-        yob: 2011,
+        gender: 'f',
         age: -1
     }
 ];
 
 for (var current in children) {
-    children[current].age = howOld(children[current].yob);
+    children[current].age = howOld(
+        children[current]._yob,
+        children[current]._mob
+    );
 }
 
 console.log(children);
@@ -36,4 +71,7 @@ var name = 'Julia', // string aka text
 console.log('');
 console.log('Hello ' + name + ' you are ' + yearsOld + ' years old.');
 console.log('');
-
+console.log('Summer camp for 3-5 girls');
+console.log(summerCourseForGirls3to5yo(children));
+console.log('Summer camp for 5-11 boys');
+console.log(summerCourseForBoys5to11(children));
