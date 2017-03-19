0d68aa6db0fa9b50fe6d992932a93af63164d21d
Martin Swiderski <codebloke@gmail.com>
2017-03-19 10:12:20 +0000

    age calc imprecise

--- test.js
+++ test.js
@@ -1,5 +1,34 @@
 
 
+
+function howOld(born) {
+    return (new Date).getFullYear() - born;
+}
+
+var children = [ // array
+    {
+        name: 'Julia',
+        yob: 2007,
+        age: -1
+    },
+    {
+        name: 'Leon',
+        yob: 2009,
+        age: -1
+    },
+    {
+        name: 'Rita',
+        yob: 2011,
+        age: -1
+    }
+];
+
+for (var current in children) {
+    children[current].age = howOld(children[current].yob);
+}
+
+console.log(children);
+
 var name = 'Julia', // string aka text
     yob = 2007,     // integer number
     yearsOld = (new Date).getFullYear() - yob;
