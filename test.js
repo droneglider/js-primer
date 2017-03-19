0252954f903564cbfa2a9382f895f676d74c401b
Martin Swiderski <codebloke@gmail.com>
2017-03-19 10:01:56 +0000

    version-1

---
+++ test.js
@@ -0,0 +1,10 @@
+
+
+var name = 'Julia', // string aka text
+    yob = 2007,     // integer number
+    yearsOld = (new Date).getFullYear() - yob;
+
+console.log('');
+console.log('Hello ' + name + ' you are ' + yearsOld + ' years old.');
+console.log('');
+
